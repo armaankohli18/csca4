c5741af65b04209855c3a97275af21dc6b6cdf26
armaankohli18 <armaankcollege@gmail.com>
2026-02-23 16:24:48 -0800

    wrote intlist

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import sys
 import string
 sys.setrecursionlimit(10**6)
 
+IntList: TypeAlias = Union[None, "IntNode"]
+
+@dataclass(frozen=True)
+class IntNode:
+  first: int
+  rest: IntList
+
 class Tests(unittest.TestCase):
   pass
   
